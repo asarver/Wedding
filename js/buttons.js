9231c1b8e441e73033ffc4b660c9f9fc078601c1
Ashley Sarver <asarver1@gmail.com>
2014-04-26 23:26:52 -0400

    added maps for locations

--- js/buttons.js
+++ js/buttons.js
@@ -17,35 +17,13 @@ $(function() {
 		$(id).toggleClass("current-tab");
 		
 		if (id == "#ceremony") {
-		  var lat = 29.6416124;
-			var lon = -82.3638806;
-			set_map(lat, lon, 'Baughman Center', 'baughman_map');
+		  var iframe = document.getElementById("baughman_iframe");
+      iframe.src = iframe.src;
 		}
 		
 		if (id == "#reception") {
-		  var lat = 29.6495629;
-			var lon = -82.3239809;
-			set_map(lat, lon, 'The Hippodrome', 'hipp_map');
+		  var iframe = document.getElementById("hipp_iframe");
+      iframe.src = iframe.src;
 		}
-		
-		
 	});
-	
-	function set_map(lat, lon, loc_name, id) {
-	  var map_canvas = document.getElementById(id);
-		var map_options = {
-			center: new google.maps.LatLng(lat,lon),
-			zoom: 18,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-		var map = new google.maps.Map(map_canvas, map_options)
-		var marker = new google.maps.Marker({
-			position: new google.maps.LatLng(lat,lon),
-			map: map,
-			title: loc_name
-		});
-	}
-	
-	
-      
 });
